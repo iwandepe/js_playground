6d4b5383a64cc5d5d141d8a831b2e523ae3dda42
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2022-01-04 20:29:38 +0700

    fix(nest_postgres): migration config

--- nest-postgres/ormconfig.js
+++ nest-postgres/ormconfig.js
@@ -1,13 +1,15 @@
 module.exports = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'root', // change to your username
-    password: 'root', // change to your password
-    database: 'nest_postgres',
-    // migrations: ['migrations/**/*{.ts,.js}'],
-    cli: {
-        entitiesDir: 'src',
-        migrationsDir: 'migrations',
-    },
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres', // change to your username
+  password: 'securepass', // change to your password
+  database: 'nest_postgres',
+  migrations: [
+    process.env.APP_ENV === 'migration' ? 'migrations/**/*{.ts,.js}' : '',
+  ],
+  cli: {
+    entitiesDir: 'src',
+    migrationsDir: 'migrations',
+  },
 };
